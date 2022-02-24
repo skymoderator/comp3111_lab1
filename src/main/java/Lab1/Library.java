aa715153f12b4eeb0c92438f4e5b09414e56da17
William <williamchoi12175@gmail.com>
2022-02-24 17:49:44 +0800

    Made some changes

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,8 +6,10 @@ package Lab1;
 public class Library {
 
     /* Add this function */
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String arg[]) {
+        Library lib = new Library();
+        if (lib.someLibraryMethod())
+            System.out.println("Hello, World!");
     }
 
     public boolean someLibraryMethod() {
